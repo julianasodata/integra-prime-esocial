3ceab864f279dff4851e1a3a33f8366e934781c5
Juliana Santos <juliana.snto.oliveira@gmail.com>
2026-01-30 19:41:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/integra-prime-esocial/" : "/",
-
+  base: mode === "production" ? "/" : "/",
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
